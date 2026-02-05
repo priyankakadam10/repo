d60f3e7a45ea9f07d8aad8afcdd5baa33c8e239c
priyankakadam10 <priya1110kadam@gmail.com>
2026-02-06 05:07:44 +0530

    Improve error handling and response output
    Updated error handling and response printing for GitHub API requests.

--- app.py
+++ app.py
@@ -6,26 +6,30 @@ owner = "priyankakadam10"
 Repo = "repo"
 Branch = "main"
 
+
 input_path = "input/Test.txt"
 output_path = "output/Test.txt"
 
 Headers ={
-  "Authorization":f"token{GitHub_Token}",
+  "Authorization": f"token{GitHub_Token}",
   "Accept":"application/vnd.github.v3+json"
 }
 
 get_url = f"https://api.github.com/repos/{owner}/{Repo}/contents/{input_path}"
 
+
 response = requests.get(get_url,headers=Headers)
 
-if response.status_code != 200:
-  raise Exception("Failed to read the file")
+print("Status:",response.status_code)
+print("Response:",response.text)
 
-file_data = response.json()
-file_content = base64.b64decode(file_data["content"]).decode("utf-8")
-file_sha = file_data["company"]
-
-print("File read successfully from input")
+if response.status_code == 200:
+#raise Exception("Failed to read the file")
+  file_data = response.json()
+  file_content = base64.b64decode(file_data["content"]).decode()
+  #file_sha = file_data["company"]
+  print("File_content:")
+  print(file_content)
 
 put_url = f"https://api.github.com/repos/{owner}/{Repo}/contents/{output_path}"
 
